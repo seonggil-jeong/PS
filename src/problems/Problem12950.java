1467a8e2fb610b8fc923af1323f22edd02b8ba46
JeongSeonggil <dataofsg02@gmail.com>
2022-08-20 03:37:42 +0900

    [/Problems/행렬의 덧셈] feature : https://school.programmers.co.kr/learn/courses/30/lessons/12950

---
+++ src/problems/Problem12950.java
@@ -0,0 +1,24 @@
+package problems;
+
+public class Problem12950 {
+    public static void main(String[] args) {
+        final int[][] arr1 = {{1, 2}, {2, 3}};
+        final int[][] arr2 = {{3, 4}, {5, 6}};
+
+        System.out.println(solution(arr1, arr2));
+
+    }
+
+
+    private static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
